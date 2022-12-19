2553eb6ee289c2a25cbb5b80227e34b53587e3ba
junta <j1u1n5@gmail.com>
2022-12-19 14:31:19 +0900

    商品一覧ページ遷移

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
@@ -19,5 +20,5 @@ class ProductsController < ApplicationController
 
   def destroy
   end
-  
+
 end
--- db/migrate/20221219024236_create_products.rb
+++ db/migrate/20221219024236_create_products.rb
@@ -1,12 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.integer :company_id,     null: false
+      t.integer :company_id      
       t.string :product_name,     null: false
-      t.integer :price,     null: false
-      t.stock :integer,     null: false
-      t.text :comment,     null: false
-      t.text :image_path,     null: false
+      t.integer :price,           null: false
+      t.integer :stock,           null: false
+      t.text :comment,            null: false
+      t.text :image_path
 
 
       t.timestamps
