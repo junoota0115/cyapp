9c14bdfd8de5aa7361cd461ff8cf2dd838534ab0
junta <j1u1n5@gmail.com>
2022-12-20 11:30:10 +0900

    商品編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,12 +16,21 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+  
+  def edit
+    @product = Product.find(params[:id])
+  end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
   end
 
-  def edit
-  end
+
 
   def destroy
     @product = Product.find(params[:id])
